cf333cead68892dddb30579c88830b386eb26802
razzak118348 <abdurrazzak118348@gmail.com>
2024-02-26 03:41:14 +0600

    find the press key and random key match or not

--- script/alpha_clash.js
+++ script/alpha_clash.js
@@ -11,55 +11,85 @@
 // }
 
 // way 2nd by utility 
-function play(){
-   hideElementById('home');
-   showElementById('play-ground');
-   continueGame();
+function play() {
+    hideElementById('home');
+    showElementById('play-ground');
+    continueGame();
 }
 
-function hideElementById(elementId){
-    const element=document.getElementById(elementId);
+function hideElementById(elementId) {
+    const element = document.getElementById(elementId);
     element.classList.add('hidden')
 }
 
-function showElementById(elementId){
-    const element=document.getElementById(elementId)
+function showElementById(elementId) {
+    const element = document.getElementById(elementId)
     element.classList.remove('hidden')
 
 }
 
-function setBackgruondColor(elementId){
-    const element=document.getElementById(elementId);
+function setBackgruondColor(elementId) {
+    const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400');
-  
+
 }
-function removeBackgroundColor(elementId){
-    const element=document.getElementById(elementId);
+function removeBackgroundColor(elementId) {
+    const element = document.getElementById(elementId);
     element.classList.remove('bg-orange-400');
 }
 // utility end 
 
+// to match the keyboard key and random alphabet 
+function handleKeyboardButtonPress(event){
+    // console.log(event)// if we do console.log(event)then we find the all the property of the event ...but we need key >>> so that we can match the return alphabet ...
+    // console.log(event.key)//we find the keyboard key ;
+
+    const playerPressed = event.key;
+    console.log('press key', playerPressed);
+    //get the expected to press
+    const currentAlphabetElement=document.getElementById('letter');
+    const currentAlphabet=currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+
+    console.log('the alphabet is ', expectedAlphabet);
+    console.log(playerPressed,expectedAlphabet)
+
+    if(playerPressed===expectedAlphabet){
+        console.log('win')
+    }
+    else{
+        console.log('loss')
+    }
+  
+}
+document.addEventListener('keyup', handleKeyboardButtonPress)
 
 
-function continueGame(){
+function continueGame() {
     //step1: generate a random alphabet
-    const alphabet=getARandomAlphabet();
+    const alphabet = getARandomAlphabet();
     // console.log('your random Alphabet is :', alphabet);
     // set random alphabet to display 
-    const currentAlphabet=document.getElementById('letter');
-    currentAlphabet.innerText=alphabet;
+    const currentAlphabet = document.getElementById('letter');
+    currentAlphabet.innerText = alphabet;
+    // console.log('continuegame alphabet',alphabet)
 
+   
     //set background color:
     setBackgruondColor(alphabet);
+
+    return alphabet;
 }
 
-function getARandomAlphabet(){
-    const alphabetString='abcdefdghijklmnopqrstuvwxyz'
-    const alphabets=alphabetString.split('');//find single cherecter
+
+
+function getARandomAlphabet() {
+    const alphabetString = 'abcdefdghijklmnopqrstuvwxyz'
+    const alphabets = alphabetString.split('');//find single cherecter
     //generate random number 0 to 25
-   const  randomNumber= 0 + Math.random()*25;
-   const indexOfArray=Math.round(randomNumber);
-   const alphabet=alphabets[indexOfArray];
+    const randomNumber = 0 + Math.random() * 25;
+    const indexOfArray = Math.round(randomNumber);
+    const alphabet = alphabets[indexOfArray];
 
     return alphabet;
 }
