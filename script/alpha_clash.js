9db6ad59c4e9340e3aec26578ad9acb8b125dbf8
razzak118348 <abdurrazzak118348@gmail.com>
2024-02-26 15:55:13 +0600

    the score and escape and gameover section

--- script/alpha_clash.js
+++ script/alpha_clash.js
@@ -12,10 +12,21 @@
 
 // way 2nd by utility 
 function play() {
+    //hide everything show only  the play ground
     hideElementById('home');
     showElementById('play-ground');
+    hideElementById('final-score');
+
+    //reset score and life
+setTextElementValueById('current-life',5);
+setTextElementValueById('score',0)
+
+//continue game ;
     continueGame();
+
 }
+
+
 
 function hideElementById(elementId) {
     const element = document.getElementById(elementId);
@@ -37,6 +48,25 @@ function removeBackgroundColor(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('bg-orange-400');
 }
+
+function getTextElemetnValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+function getElementTextById(elementId){
+const element = document.getElementById(elementId);
+const text= element.innerText;
+return text ;
+}
+
 // utility end 
 
 // to match the keyboard key and random alphabet 
@@ -44,29 +74,68 @@ function handleKeyboardButtonPress(event) {
 
     // console.log(event)// if we do console.log(event)then we find the all the property of the event ...but we need key >>> so that we can match the return alphabet ...
     // console.log(event.key)//we find the keyboard key 
-    const playerPressed=event.key;
-    console.log('press key', playerPressed);
+    const playerPressed = event.key;
+    console.log('press key by plear', playerPressed);
+
+    //stop the game if pressed 'Esc' key
+    if(playerPressed === 'Escape'){
+        gameOver();
+    }
+
     //get the expected to press
-    const currentAlphabetElement=document.getElementById('letter');
-    const currentAlphabet=currentAlphabetElement.innerText;
-    const expectedAlphabet=currentAlphabet.toLowerCase();
+    const currentAlphabetElement = document.getElementById('letter');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
 
     console.log('the alphabet is ', expectedAlphabet);
-  
-    if(playerPressed === expectedAlphabet){
+
+    if (playerPressed === expectedAlphabet) {
         console.log('you got a point');
-        console.log('you have pressed currently',expectedAlphabet);
+
+        //     // update score: 
+        //     // 1.get the current score :
+        //     const currentScoreId=document.getElementById('score');
+        //     const score=currentScoreId.innerText;
+        //    const  currentScore=parseInt(score);
+        //     //2. increasee the score by 1 
+        //    const newScore=currentScore + 1;
+        //     //show the update score
+        //     currentScoreId.innerText=newScore;
+
+        // 2nd way by function 
+        const currentScore = getTextElemetnValueById('score');
+        const newScore = currentScore + 1;
+        //set the new score
+        setTextElementValueById('score', newScore);
+
+        //start a new round
         removeBackgroundColor(expectedAlphabet);
         continueGame();
     }
 
-    else{
-        console.log('you press wrong');
+    else {
+        //    1.get the current life
+        // const currentLifeElement=document.getElementById('current-life')
+        // const currentLife=currentLifeElement.innerText;
+        // const life=parseInt(currentLife);
+        //     //2.reduce the life count
+        // const countLife=life - 1;
+        //     //3.show the life count
+        //     currentLifeElement.innerText=countLife;
+
+        // 2nd way by function 
+        const currentLife = getTextElemetnValueById('current-life')
+        const newLife = currentLife - 1;
+
+        setTextElementValueById('current-life', newLife);
+        if (newLife === 0) {
+            gameOver();
+        }
+
     }
 
-
 }
-document.addEventListener('keyup',handleKeyboardButtonPress)
+document.addEventListener('keyup', handleKeyboardButtonPress)
 
 
 function continueGame() {
@@ -76,7 +145,7 @@ function continueGame() {
     // set random alphabet to display 
     const currentAlphabet = document.getElementById('letter');
     currentAlphabet.innerText = alphabet;
- console.log('continuegame alphabet', alphabet)
+    console.log('continuegame alphabet', alphabet)
 
 
     //set background color:
@@ -96,4 +165,19 @@ function getARandomAlphabet() {
     const alphabet = alphabets[indexOfArray];
 
     return alphabet;
+}
+
+function gameOver() {
+    hideElementById('play-ground');
+    showElementById('final-score');
+
+    // update final score 
+    const lastScore=getTextElemetnValueById('score');
+    setTextElementValueById('total-score',lastScore);
+
+    //clear the last selected alphabet;
+    const alphabet=getElementTextById('letter');
+
+    //remove background color
+    removeBackgroundColor(alphabet);
 }
